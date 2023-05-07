fbbbaff29464371692ea94703729322ccc5838d3
LeeSeungjae00 <seungjae266789@gmail.com>
2023-05-07 12:22:31 +0900

    :wrench: Update configuration files.
    cors 설정 config 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "http://mingky.me:22001/:path*",
+      },
+    ];
+  },
+}
 
 module.exports = nextConfig
