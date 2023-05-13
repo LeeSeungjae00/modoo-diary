5aa7b2dc8ee4c8f6cece87b4d168de009d564ed3
LeeSeungjae00 <seungjae266789@gmail.com>
2023-05-13 22:20:17 +0900

    :lipstick: Add UI
    일기 보기 list UI 간단 틀 완성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
 "use client";
 import { getDiarys } from "@/api/diary";
 import { API_ROUTE_DIARIES_GET } from "@/constants/api/diary";
+import styled from "@emotion/styled";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
 export type Products = {
   products: number[];
 };
+
+const DiaryDiv = styled.div`
+  font-family: Chilgok_lws;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: flex-start;
+  padding: 0.5rem;
+`;
 
 export default function Home() {
   const query = useInfiniteQuery({
@@ -22,7 +32,7 @@ export default function Home() {
   });
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <button
         onClick={() => {
           query.fetchNextPage();
@@ -30,12 +40,26 @@ export default function Home() {
       >
         go
       </button>
+
       {query.data &&
-        JSON.stringify(
-          query.data.pages.reduce((prev: any[], curr) => {
+        query.data.pages
+          .reduce((prev: any[], curr) => {
             return [...prev, ...curr.data.data.content];
           }, [])
-        )}
+          .map((diary) => {
+            return (
+              <DiaryDiv key={diary.id}>
+                <div className="flex justify-between">
+                  <p className="text-lg">{diary.nickName}의 일기</p>
+                </div>
+                <div>
+                  <p className="pb-1">제목 : {diary.title}</p>
+                  <p>오늘날씨 {diary.weather}</p>
+                  <p>{diary.content}</p>
+                </div>
+              </DiaryDiv>
+            );
+          })}
     </main>
   );
 }
