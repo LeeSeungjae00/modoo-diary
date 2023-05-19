ea03d3076662daa4063c51e6f893d5a88669dab6
LeeSeungjae00 <seungjae266789@gmail.com>
2023-05-19 17:36:14 +0900

    :mag: Improve SEO.
    Next 13 은 metaData 객체안에 넣어주면 알아서 해주네 layout에

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,18 @@ import { Inter } from "next/font/google";
 import Header from "@/components/header";
 import ReactQueryProvider from "./reactQueryProvider";
 import { AuthContextProvider } from "@/context/authInfo.context";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "모두의 일기",
   description: "모두 다같이 일기를 작성해봐요.",
-  ogImage: "/images/welldone.png",
-  ogUrl: "https://modoo-diary.vercel.app/",
+  openGraph: {
+    images: "/images/welldone.png",
+    description: "모두 다같이 일기를 작성해봐요.",
+    url: "https://modoo-diary.vercel.app/",
+  },
 };
 
 export default function RootLayout({
@@ -21,14 +25,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description}></meta>
-        <meta property="og:title" content="모두의 일기"></meta>
-        <meta property="og:image" content={metadata.ogImage}></meta>
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:url" content={metadata.ogUrl}></meta>
-      </Head>
       <body className={inter.className}>
         <AuthContextProvider>
           <Header></Header>
