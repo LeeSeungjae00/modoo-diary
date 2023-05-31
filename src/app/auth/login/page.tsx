56b480861bbca37d3ed5c3ee69bb36a5a51045e5
LeeSeungjae00 <seungjae266789@gmail.com>
2023-05-31 22:35:28 +0900

    :lipstick: 회원가입 로그인 처럼 가운대 정렬 폼 layout 생성 후 적용

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -37,102 +37,94 @@ export default function SignIn() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center px-6 py-28 mx-auto min-h-screen lg:py-0">
-      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-            모두의 일기에 로그인 하세요
-          </h1>
-          <form
-            onSubmit={handleSubmit(onSubmitSignUp, (data) => {
-              console.log(data);
+    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+      <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+        모두의 일기에 로그인 하세요
+      </h1>
+      <form
+        onSubmit={handleSubmit(onSubmitSignUp, (data) => {
+          console.log(data);
+        })}
+        className="space-y-4 md:space-y-6"
+        action="#"
+      >
+        <div>
+          <Label htmlFor="loginId">아이디</Label>
+          <Input
+            {...register("loginId", {
+              required: "아이디를 입력해주세요",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "이메일 형식에 맞지 않습니다.",
+              },
             })}
-            className="space-y-4 md:space-y-6"
-            action="#"
-          >
-            <div>
-              <Label htmlFor="loginId">아이디</Label>
-              <Input
-                {...register("loginId", {
-                  required: "아이디를 입력해주세요",
-                  pattern: {
-                    value: /\S+@\S+\.\S+/,
-                    message: "이메일 형식에 맞지 않습니다.",
-                  },
-                })}
-                type="loginId"
-                name="loginId"
-                id="loginId"
-                placeholder="name@company.com"
-              />
-            </div>
-            {errors.loginId && (
-              <InputAlert
-                message={errors.loginId.message as string}
-              ></InputAlert>
-            )}
-            <div>
-              <Label htmlFor="password">비밀번호</Label>
-              <Input
-                {...register("password", {
-                  required: "비밀번호를 입력해주세요",
-                  pattern: {
-                    value: /^[\da-zA-Z!@#]{4,}$/,
-                    message: "비밀번호는 최소 4자 이상입니다.",
-                  },
-                })}
-                type="password"
-                name="password"
-                id="password"
-                placeholder="••••••••"
-              />
-            </div>
-            {errors.password && (
-              <InputAlert
-                message={errors.password.message as string}
-              ></InputAlert>
-            )}
-            <div className="flex items-center justify-between">
-              <div className="flex items-start">
-                <div className="flex items-center h-5">
-                  <input
-                    id="remember"
-                    aria-describedby="remember"
-                    type="checkbox"
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                  />
-                </div>
-                <div className="ml-3 text-sm">
-                  <label
-                    htmlFor="remember"
-                    className="text-gray-500 dark:text-gray-300"
-                  >
-                    아이디 정보 기억하기
-                  </label>
-                </div>
-              </div>
-            </div>
-            <button
-              type="submit"
-              className="w-full text-blue-800 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-            >
-              일기 쓰러 가기
-            </button>
-            {dataFetchError && (
-              <InputAlert message="아이디와 비밀번호를 확인해 주세요."></InputAlert>
-            )}
-            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-              계정이 없으신가요?{" "}
-              <Link
-                href="/auth/signup"
-                className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-              >
-                회원 가입
-              </Link>
-            </p>
-          </form>
+            type="loginId"
+            name="loginId"
+            id="loginId"
+            placeholder="name@company.com"
+          />
         </div>
-      </div>
+        {errors.loginId && (
+          <InputAlert message={errors.loginId.message as string}></InputAlert>
+        )}
+        <div>
+          <Label htmlFor="password">비밀번호</Label>
+          <Input
+            {...register("password", {
+              required: "비밀번호를 입력해주세요",
+              pattern: {
+                value: /^[\da-zA-Z!@#]{4,}$/,
+                message: "비밀번호는 최소 4자 이상입니다.",
+              },
+            })}
+            type="password"
+            name="password"
+            id="password"
+            placeholder="••••••••"
+          />
+        </div>
+        {errors.password && (
+          <InputAlert message={errors.password.message as string}></InputAlert>
+        )}
+        <div className="flex items-center justify-between">
+          <div className="flex items-start">
+            <div className="flex items-center h-5">
+              <input
+                id="remember"
+                aria-describedby="remember"
+                type="checkbox"
+                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+              />
+            </div>
+            <div className="ml-3 text-sm">
+              <label
+                htmlFor="remember"
+                className="text-gray-500 dark:text-gray-300"
+              >
+                아이디 정보 기억하기
+              </label>
+            </div>
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="w-full text-blue-800 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+        >
+          일기 쓰러 가기
+        </button>
+        {dataFetchError && (
+          <InputAlert message="아이디와 비밀번호를 확인해 주세요."></InputAlert>
+        )}
+        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+          계정이 없으신가요?{" "}
+          <Link
+            href="/auth/signup"
+            className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+          >
+            회원 가입
+          </Link>
+        </p>
+      </form>
     </div>
   );
 }
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -36,168 +36,154 @@ export default function SignUp() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center px-6 py-28 mx-auto min-h-screen lg:py-0 ">
-      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-            모두의 일기에 가입하세요
-          </h1>
-          <form
-            onSubmit={handleSubmit(onSubmitSignUp, (data) => {
-              console.log(data);
+    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+      <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+        모두의 일기에 가입하세요
+      </h1>
+      <form
+        onSubmit={handleSubmit(onSubmitSignUp, (data) => {
+          console.log(data);
+        })}
+        className="space-y-4 md:space-y-6"
+        action="#"
+      >
+        <div>
+          <Label htmlFor="loginId">아이디</Label>
+          <Input
+            {...register("loginId", {
+              required: "아이디를 입력해주세요",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "이메일 형식에 맞지 않습니다.",
+              },
             })}
-            className="space-y-4 md:space-y-6"
-            action="#"
-          >
-            <div>
-              <Label htmlFor="loginId">아이디</Label>
-              <Input
-                {...register("loginId", {
-                  required: "아이디를 입력해주세요",
-                  pattern: {
-                    value: /\S+@\S+\.\S+/,
-                    message: "이메일 형식에 맞지 않습니다.",
-                  },
-                })}
-                type="loginId"
-                name="loginId"
-                id="loginId"
-                placeholder="name@company.com"
-              />
-            </div>
-            {errors.loginId && (
-              <InputAlert
-                message={errors.loginId.message as string}
-              ></InputAlert>
-            )}
-            <div>
-              <Label htmlFor="password">비밀번호</Label>
-              <Input
-                {...register("password", {
-                  required: "비밀번호를 입력해주세요",
-                  pattern: {
-                    value: /^[\da-zA-Z!@#]{4,}$/,
-                    message: "비밀번호는 최소 4자 이상입니다.",
-                  },
-                })}
-                type="password"
-                name="password"
-                id="password"
-                placeholder="••••••••"
-              />
-            </div>
-            {errors.password && (
-              <InputAlert
-                message={errors.password.message as string}
-              ></InputAlert>
-            )}
-            <div>
-              <Label htmlFor="passwordConfirmation">비밀번호 확인</Label>
-              <Input
-                {...register("passwordConfirmation", {
-                  required: "비밀번호를 확인해주세요",
-                  validate: {
-                    matchPreviousPassword: (value) => {
-                      const { password } = getValues();
-                      return (
-                        password === value || "비밀번호가 일치하지 않습니다"
-                      );
-                    },
-                  },
-                })}
-                type="password"
-                name="passwordConfirmation"
-                id="passwordConfirmation"
-                placeholder="••••••••"
-              />
-            </div>
-            {errors.passwordConfirmation && (
-              <InputAlert
-                message={errors.passwordConfirmation.message as string}
-              ></InputAlert>
-            )}
-            <div>
-              <Label htmlFor="nickName">닉네임</Label>
-              <Input
-                {...register("nickName", { required: "닉네임을 입력해주세요" })}
-                type="nickName"
-                name="nickName"
-                id="nickName"
-                placeholder="모두의 일기"
-              />
-            </div>
-            {errors.nickName && (
-              <InputAlert
-                message={errors.nickName.message as string}
-              ></InputAlert>
-            )}
-            <div>
-              <Label htmlFor="region">지역</Label>
-              <Radio
-                radioList={[
-                  {
-                    inputArg: {
-                      value: "SEOUL",
-                      ...register("region", {
-                        required: "지역을 선택해 주세요",
-                      }),
-                    },
-                    lable: "서울",
-                  },
-                  {
-                    inputArg: {
-                      value: "BUSAN",
-                      ...register("region", {
-                        required: "지역을 선택해 주세요",
-                      }),
-                    },
-                    lable: "부산",
-                  },
-                  {
-                    inputArg: {
-                      value: "INCHEON",
-                      ...register("region", {
-                        required: "지역을 선택해 주세요",
-                      }),
-                    },
-                    lable: "인천",
-                  },
-                  {
-                    inputArg: {
-                      value: "ULSAN",
-                      ...register("region", {
-                        required: "지역을 선택해 주세요",
-                      }),
-                    },
-                    lable: "울산",
-                  },
-                  {
-                    inputArg: {
-                      value: "GWANGJU",
-                      ...register("region", {
-                        required: "지역을 선택해 주세요",
-                      }),
-                    },
-                    lable: "광주",
-                  },
-                ]}
-              ></Radio>
-            </div>
-            {errors.region && (
-              <InputAlert
-                message={errors.region.message as string}
-              ></InputAlert>
-            )}
-            <button
-              type="submit"
-              className="w-full text-blue-800 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-            >
-              회원가입 하고 로그인 하러 가기
-            </button>
-            {errorMessage && <InputAlert message={errorMessage}></InputAlert>}
-          </form>
+            type="loginId"
+            name="loginId"
+            id="loginId"
+            placeholder="name@company.com"
+          />
         </div>
-      </div>
+        {errors.loginId && (
+          <InputAlert message={errors.loginId.message as string}></InputAlert>
+        )}
+        <div>
+          <Label htmlFor="password">비밀번호</Label>
+          <Input
+            {...register("password", {
+              required: "비밀번호를 입력해주세요",
+              pattern: {
+                value: /^[\da-zA-Z!@#]{4,}$/,
+                message: "비밀번호는 최소 4자 이상입니다.",
+              },
+            })}
+            type="password"
+            name="password"
+            id="password"
+            placeholder="••••••••"
+          />
+        </div>
+        {errors.password && (
+          <InputAlert message={errors.password.message as string}></InputAlert>
+        )}
+        <div>
+          <Label htmlFor="passwordConfirmation">비밀번호 확인</Label>
+          <Input
+            {...register("passwordConfirmation", {
+              required: "비밀번호를 확인해주세요",
+              validate: {
+                matchPreviousPassword: (value) => {
+                  const { password } = getValues();
+                  return password === value || "비밀번호가 일치하지 않습니다";
+                },
+              },
+            })}
+            type="password"
+            name="passwordConfirmation"
+            id="passwordConfirmation"
+            placeholder="••••••••"
+          />
+        </div>
+        {errors.passwordConfirmation && (
+          <InputAlert
+            message={errors.passwordConfirmation.message as string}
+          ></InputAlert>
+        )}
+        <div>
+          <Label htmlFor="nickName">닉네임</Label>
+          <Input
+            {...register("nickName", { required: "닉네임을 입력해주세요" })}
+            type="nickName"
+            name="nickName"
+            id="nickName"
+            placeholder="모두의 일기"
+          />
+        </div>
+        {errors.nickName && (
+          <InputAlert message={errors.nickName.message as string}></InputAlert>
+        )}
+        <div>
+          <Label htmlFor="region">지역</Label>
+          <Radio
+            radioList={[
+              {
+                inputArg: {
+                  value: "SEOUL",
+                  ...register("region", {
+                    required: "지역을 선택해 주세요",
+                  }),
+                },
+                lable: "서울",
+              },
+              {
+                inputArg: {
+                  value: "BUSAN",
+                  ...register("region", {
+                    required: "지역을 선택해 주세요",
+                  }),
+                },
+                lable: "부산",
+              },
+              {
+                inputArg: {
+                  value: "INCHEON",
+                  ...register("region", {
+                    required: "지역을 선택해 주세요",
+                  }),
+                },
+                lable: "인천",
+              },
+              {
+                inputArg: {
+                  value: "ULSAN",
+                  ...register("region", {
+                    required: "지역을 선택해 주세요",
+                  }),
+                },
+                lable: "울산",
+              },
+              {
+                inputArg: {
+                  value: "GWANGJU",
+                  ...register("region", {
+                    required: "지역을 선택해 주세요",
+                  }),
+                },
+                lable: "광주",
+              },
+            ]}
+          ></Radio>
+        </div>
+        {errors.region && (
+          <InputAlert message={errors.region.message as string}></InputAlert>
+        )}
+        <button
+          type="submit"
+          className="w-full text-blue-800 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+        >
+          회원가입 하고 로그인 하러 가기
+        </button>
+        {errorMessage && <InputAlert message={errorMessage}></InputAlert>}
+      </form>
     </div>
   );
 }
