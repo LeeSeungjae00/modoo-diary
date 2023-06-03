1b1e82b557910fb27041e08fd63fa8b934a51b9f
LeeSeungjae00 <seungjae266789@gmail.com>
2023-06-03 09:53:21 +0900

    :sparkles: 무한스크롤 하는 client component 추가

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/:path*",
-        destination: "http://mingky.me:22001/:path*",
+        source: "/api:path*",
+        destination: "http://mingky.me:22001/api:path*",
       },
     ];
   },
---
+++ src/components/infiniteDiary.tsx
@@ -0,0 +1,107 @@
+"use client";
+import { getDiarys } from "@/api/diary";
+import { API_ROUTE_DIARIES_GET } from "@/constants/api/diary";
+import useIntersection from "@/hooks/useIntersection";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useContext, useEffect, useRef } from "react";
+import { FontSpan } from "@/components/common/fontSpan";
+import { AuthContext } from "@/context/authInfo.context";
+import DiaryDiv from "@/components/diaryDiv";
+import { useIsMobile } from "@/hooks/useIsMobile";
+
+export type Products = {
+  products: number[];
+};
+
+export default function Home() {
+  const {
+    data,
+    isSuccess,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isFetching,
+  } = useInfiniteQuery({
+    queryKey: [API_ROUTE_DIARIES_GET],
+    queryFn: ({ pageParam = 0 }) => getDiarys(pageParam),
+    getNextPageParam: (lastPage) => {
+      const nowPage = lastPage.number + 1;
+      if (nowPage < lastPage.totalPages) {
+        return nowPage;
+      }
+      return undefined;
+    },
+    select: (data) => {
+      return {
+        pageParams: data.pageParams,
+        pages: data.pages.reduce((prev: any[], curr) => {
+          return [...prev, ...curr.data];
+        }, []),
+      };
+    },
+  });
+  const fetchMoreRef = useRef<HTMLDivElement>(null);
+  const intersecting = useIntersection(fetchMoreRef);
+  const { state } = useContext(AuthContext);
+  const isMobile = useIsMobile();
+
+  useEffect(() => {
+    if (!intersecting || !isSuccess || !hasNextPage || isFetchingNextPage)
+      return;
+    fetchNextPage();
+  }, [fetchNextPage, hasNextPage, intersecting, isFetchingNextPage, isSuccess]);
+
+  return (
+<>      {data &&
+        (isMobile ? (
+          data.pages.map((diary) => {
+            return (
+              <DiaryDiv
+                key={diary.id}
+                {...diary}
+                isLogin={state.isLogin}
+              ></DiaryDiv>
+            );
+          })
+        ) : (
+          <div className="flex gap-4 w-full">
+            <div className="flex-1">
+              {data.pages
+                .filter((_, i) => i % 2 === 0)
+                .map((diary) => {
+                  return (
+                    <DiaryDiv
+                      key={diary.id}
+                      {...diary}
+                      isLogin={state.isLogin}
+                    ></DiaryDiv>
+                  );
+                })}
+            </div>
+            <div className="flex-1">
+              {data.pages
+                .filter((_, i) => i % 2 === 1)
+                .map((diary) => {
+                  return (
+                    <DiaryDiv
+                      key={diary.id}
+                      {...diary}
+                      isLogin={state.isLogin}
+                    ></DiaryDiv>
+                  );
+                })}
+            </div>
+          </div>
+        ))}
+
+      <div ref={fetchMoreRef} />
+      {(isLoading || isFetching) && (
+        <FontSpan className="pt-5">일기를 쓰고있어요...</FontSpan>
+      )}
+      {!isLoading && !hasNextPage && (
+        <p className="pt-5">🎉 모든 일기를 다 읽으셨어요.</p>
+      )}
+    </>
+  );
+}
