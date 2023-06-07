ba4aa23129a4f064ca2516fa0b92962e17ff251d
LeeSeungjae00 <seungjae266789@gmail.com>
2023-06-07 21:40:35 +0900

    :ambulance: Merge 하지 않은 부분 커밋 수정

--- src/layout/centerFormLayout.tsx
+++ src/layout/centerFormLayout.tsx
@@ -6,11 +6,7 @@ export default function CenterFormLayout({
   children: React.ReactNode;
 }) {
   return (
-<<<<<<< Updated upstream
-    <div className="flex flex-col items-center justify-center px-6 py-32 mx-auto min-h-screen lg:py-0 ">
-=======
     <div className="flex flex-col items-center justify-center px-6 py-32 mx-auto min-h-screen ">
->>>>>>> Stashed changes
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         {children}
       </div>
