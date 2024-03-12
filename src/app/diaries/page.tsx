59fdc05197070fff20d8737910e1a1ef9ec80cd9
LeeSeungjae00 <seungjae266789@gmail.com>
2024-03-13 00:41:39 +0900

    🔍 메타데이터 수정

--- src/app/diaries/page.tsx
+++ src/app/diaries/page.tsx
@@ -7,11 +7,6 @@ export type Products = {
   products: number[];
 };
 
-export const metadata = {
-  title: "모두의 일기",
-  description: "모두의 일기",
-};
-
 async function getFirstDiaries() {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_ORIGIN_SERVER}/api/diaries?offset=0`,
